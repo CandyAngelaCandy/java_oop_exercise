aed165017a8773df21a0033d5734dbc0ddb49788
Candy <229355694@qq.com>
2018-04-10 20:57:16 +0800

    finish parctice 9

--- src/main/java/practice09/Person.java
+++ src/main/java/practice09/Person.java
@@ -1,4 +1,53 @@
 package practice09;
 
 public class Person {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    private String name;
+    private int age;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj != null) {
+            Person person = (Person) obj;
+            return person.getId() == this.getId();
+        }
+        return false;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public String introduce() {
+        return "My name is " + this.name + ". I am " + this.age + " years old.";
+    }
 }
+
+
