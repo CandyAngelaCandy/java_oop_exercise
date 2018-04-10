cf87f6a4dad51b580f07be922fe325bbdb72e239
Candy <229355694@qq.com>
2018-04-10 21:24:53 +0800

    finish parctice 10

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,49 @@
 package practice10;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+    private int number;
+    private Student leader;
+    private List<Student> students = new ArrayList<>();
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + this.number;
+    }
+
+    public void assignLeader(Student leader) {
+        if (this.students.contains(leader)) {
+            this.leader = leader;
+        } else {
+            System.out.print("It is not one of us.\n");
+        }
+
+    }
+
+    public void appendMember(Student student) {
+        this.students.add(student);
+    }
+
 }
+
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,56 @@
 package practice10;
 
-public class Teacher {
+import java.util.HashSet;
+import java.util.Set;
+
+public class Teacher extends Person{
+
+    private Set<Klass> classes = new HashSet<>();
+
+    public Set<Klass> getClasses() {
+        return classes;
+    }
+
+    public void setClasses(Set<Klass> classes) {
+        this.classes = classes;
+    }
+
+
+    public Teacher(int id,String name,int age,Set<Klass> classes){
+        super(id,name,age);
+        this.classes = classes;
+    }
+
+    public Teacher(int id,String name,int age){
+        super(id,name,age);
+    }
+
+    public String introduce(){
+        if (this.classes.size() == 0) {
+            return "My name is Tom. I am 21 years old. I am a Teacher. I teach No Class.";
+        } else {
+            String allClass = "";
+            for (Klass klass : this.classes) {
+                allClass = Integer.toString( klass.getNumber())+", "+allClass;
+            }
+            allClass = allClass.substring(0,allClass.length()-2);
+            return
+                    super.introduce()+" I am a Teacher. I teach Class "+allClass+".";
+        }
+    }
+
+    public boolean isTeaching(Student student){
+        if (this.classes.contains(student.getKlass())) {
+            return true;
+        }
+        return false;
+    }
+    public String introduceWith(Student student) {
+        if(this.classes.contains(student.getKlass())){
+            return "My name is Tom. I am 21 years old. I am a Teacher. I teach "+student.getName()+".";
+        }else{
+            return "My name is Tom. I am 21 years old. I am a Teacher. I don't teach "+student.getName()+".";
+        }
+    }
+
 }
