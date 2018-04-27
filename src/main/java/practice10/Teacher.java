17703068c5271fd09edf13b068b1f6707ec61d79
Candy <229355694@qq.com>
2018-04-27 10:26:01 +0800

    fix set unordered problem

--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,7 +1,10 @@
 package practice10;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.Comparator;
 
 public class Teacher extends Person{
 
@@ -26,11 +29,15 @@ public class Teacher extends Person{
     }
 
     public String introduce(){
-        if (this.classes.size() == 0) {
+        List<Klass> sortedClasses = this.classes.stream()
+                .sorted(Comparator.comparingInt(Klass::getNumber))
+                .collect(Collectors.toList());
+
+        if (sortedClasses.size() == 0) {
             return "My name is Tom. I am 21 years old. I am a Teacher. I teach No Class.";
         } else {
             String allClass = "";
-            for (Klass  klass: this.classes) {
+            for (Klass  klass: sortedClasses) {
                 if (allClass == "") {
                     allClass = Integer.toString(klass.getNumber());
                 } else {
